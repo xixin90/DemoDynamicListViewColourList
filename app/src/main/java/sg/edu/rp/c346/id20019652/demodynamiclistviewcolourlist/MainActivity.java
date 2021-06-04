01105070988445ccc8b22f056b712875c5d53000
Liew Xi Xin <20019652@myrp.edu.sg>
2021-06-04 21:25:02 +0800

    enhansement: check whether an index exists in arrraylist

--- app/src/main/java/sg/edu/rp/c346/id20019652/demodynamiclistviewcolourlist/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20019652/demodynamiclistviewcolourlist/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     EditText etElement;
     EditText etIndexElement;
     Button btnAdd;
+    Button btnRemove;
+    Button btnUpdate;
     ListView lvColour;
     ArrayList<String> alColours;
     ArrayAdapter<String> aaColour;
@@ -28,13 +32,15 @@ public class MainActivity extends AppCompatActivity {
         etElement = findViewById(R.id.editTextColour);
         etIndexElement = findViewById(R.id.editTextIndex);
         btnAdd = findViewById(R.id.buttonAddition);
+        btnRemove = findViewById(R.id.buttonRemove);
+        btnUpdate = findViewById(R.id.buttonUpdate);
         lvColour = findViewById(R.id.listViewColour);
 
         alColours = new ArrayList<>();
         alColours.add("Red");
         alColours.add("Orange");
 
-        ArrayAdapter aaColour = new ArrayAdapter<>(MainActivity.this,
+        aaColour = new ArrayAdapter<>(MainActivity.this,
                 android.R.layout.simple_list_item_1, alColours);
         lvColour.setAdapter(aaColour);
 
@@ -43,10 +49,64 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String newColour = etElement.getText().toString();
                 int pos = Integer.parseInt(etIndexElement.getText().toString());
-                //add to arraylist
-                alColours.add(pos, newColour);
-                //call adapter to update, notify there's a data change
-                aaColour.notifyDataSetChanged();
+
+                //check if index is avail in arraylist
+                if(pos <= alColours.size()) {
+                    //add to arraylist
+                    alColours.add(pos, newColour);
+                    //call adapter to update, notify there's a data change
+                    aaColour.notifyDataSetChanged();
+                }
+                else {
+                    String strMssg = "Please enter a correct position number";
+                    Toast.makeText(MainActivity.this, strMssg, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int posRemove= Integer.parseInt(etIndexElement.getText().toString());
+
+                //check if position index is avail in arraylist
+                if(posRemove <= alColours.size()){
+                    //remove from arraylist
+                    alColours.remove(posRemove);
+                    //call adapter to update, notify there's a data change
+                    aaColour.notifyDataSetChanged();
+                }
+                else {
+                    String strMssg = "Please enter a correct position number";
+                    Toast.makeText(MainActivity.this, strMssg, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String updateColour = etElement.getText().toString();
+                int posUpdate = Integer.parseInt(etIndexElement.getText().toString());
+
+                //check if position index is avail in arraylist
+                if(posUpdate <= alColours.size()){
+                    //update to arraylist
+                    alColours.set(posUpdate, updateColour);
+                    //call adapter to update, notify there's a data change
+                    aaColour.notifyDataSetChanged();
+                }
+                else {
+                    String strMssg = "Please enter a correct position number";
+                    Toast.makeText(MainActivity.this, strMssg, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        lvColour.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String colour = alColours.get(position);
+                Toast.makeText(MainActivity.this, colour, Toast.LENGTH_SHORT).show();
+                etElement.setText(alColours.get(position));
+                etIndexElement.setText(String.valueOf(position));
             }
         });
     }
