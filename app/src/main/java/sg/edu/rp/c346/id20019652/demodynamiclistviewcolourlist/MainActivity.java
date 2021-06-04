c4441b152902e5af12ed4160a5c64e69deb1ac71
Liew Xi Xin <20019652@myrp.edu.sg>
2021-06-04 12:53:49 +0800

    user can input colour and position to add colour + display

--- app/src/main/java/sg/edu/rp/c346/id20019652/demodynamiclistviewcolourlist/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20019652/demodynamiclistviewcolourlist/MainActivity.java
@@ -3,12 +3,51 @@ package sg.edu.rp.c346.id20019652.demodynamiclistviewcolourlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etElement;
+    EditText etIndexElement;
+    Button btnAdd;
+    ListView lvColour;
+    ArrayList<String> alColours;
+    ArrayAdapter<String> aaColour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etElement = findViewById(R.id.editTextColour);
+        etIndexElement = findViewById(R.id.editTextIndex);
+        btnAdd = findViewById(R.id.buttonAddition);
+        lvColour = findViewById(R.id.listViewColour);
+
+        alColours = new ArrayList<>();
+        alColours.add("Red");
+        alColours.add("Orange");
+
+        ArrayAdapter aaColour = new ArrayAdapter<>(MainActivity.this,
+                android.R.layout.simple_list_item_1, alColours);
+        lvColour.setAdapter(aaColour);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newColour = etElement.getText().toString();
+                int pos = Integer.parseInt(etIndexElement.getText().toString());
+                //add to arraylist
+                alColours.add(pos, newColour);
+                //call adapter to update, notify there's a data change
+                aaColour.notifyDataSetChanged();
+            }
+        });
     }
 }
